bad700ccfad4190480fff3413e99a689296fe565
Sargis Sargsyan <sargiss987@gmail.com>
2021-12-12 22:34:18 +0400

    adding repositories.

---
+++ src/main/java/guru/springframework/recipe_app/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.recipe_app.repositories;
+
+import guru.springframework.recipe_app.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category,Long> {
+}
---
+++ src/main/java/guru/springframework/recipe_app/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.recipe_app.repositories;
+
+import guru.springframework.recipe_app.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+}
